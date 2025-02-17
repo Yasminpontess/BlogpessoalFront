b64880faf343cc80142716c2aca1407a7e15c04c
Yasminpontess <161251647+Yasminpontess@users.noreply.github.com>
2025-02-17 11:57:02 -0300

    Temas

--- src/services/Services.ts
+++ src/services/Services.ts
@@ -6,14 +6,50 @@ const api = axios.create({
 
 export const cadastrarUsuario = async (
   url: string,
-  dados: Object,
+  dados: object,
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
   setDados: Function
 ) => {
   const resposta = await api.post(url, dados);
   setDados(resposta.data);
 };
 
-export const login = async (url: string, dados: Object, setDados: Function) => {
+// eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
+export const login = async (url: string, dados: object, setDados: Function) => {
   const resposta = await api.post(url, dados);
   setDados(resposta.data);
+};
+
+export const buscar = async (
+  url: string,
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
+  setDados: Function,
+  header: object
+) => {
+  const resposta = await api.get(url, header);
+  setDados(resposta.data);
+};
+
+export const cadastrar = async (
+  url: string,
+  dados: Object,
+  setDados: Function,
+  header: Object
+) => {
+  const resposta = await api.post(url, dados, header);
+  setDados(resposta.data);
+};
+
+export const atualizar = async (
+  url: string,
+  dados: Object,
+  setDados: Function,
+  header: Object
+) => {
+  const resposta = await api.put(url, dados, header);
+  setDados(resposta.data);
+};
+
+export const deletar = async (url: string, header: Object) => {
+  await api.delete(url, header);
 };
